75d356c468db125d67407bd46322684bdf42676d
Débora Moura <118219650+Debyy-moura@users.noreply.github.com>
2023-09-30 09:05:50 -0300

    Add files via upload

---
+++ Hina.java
@@ -0,0 +1,56 @@
+package Debora;
+import robocode.*;
+import java.awt.Color;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
+
+/**
+ * Hina - a robot by (your name here)
+ */
+public class Hina extends Robot
+{
+	/**
+	 * run: Hina's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// After trying out your robot, try uncommenting the import at the top,
+		// and the next line:
+
+	 setColors(Color.PINK, Color.BLACK, Color.PINK, new Color(200,25,0), new Color(150,150,200));
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			back(100);
+			turnGunRight(360);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// Replace the next line with any behavior you would like
+		fire(1);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		// Replace the next line with any behavior you would like
+		back(10);
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall(HitWallEvent e) {
+		// Replace the next line with any behavior you would like
+		back(20);
+	}	
+}
